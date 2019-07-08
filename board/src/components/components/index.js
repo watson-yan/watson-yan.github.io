7a987544067773894081215ea84fb0a81849890e
yanhua <watson-y@outlook.com>
2019-07-08 10:26:36 +0800

    删除垃圾文件

--- board/src/components/components/index.js
+++
@@ -1,9 +0,0 @@
-import gallery from './gallery.vue'
-import slider from './slider.vue'
-import pagination from './pagination.vue'
-
-export default Vue => {
-    Vue.component('gallery', gallery)
-    Vue.component('slider', slider)
-    Vue.component('pagination', pagination)
-}
